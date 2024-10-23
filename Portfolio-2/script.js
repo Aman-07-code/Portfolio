19f6b9c144e96130bf63206542466b447b1cccca
AMAN SINGH BISHT <amansinghbisht6@gmail.com>
2024-10-23 12:48:30 +0530

    Form

--- Portfolio-2/script.js
+++ Portfolio-2/script.js
@@ -15,3 +15,19 @@ window.onscroll = () =>{
     menu.remove.toggle('bx-x');
     navlist.remove.toggle('active');
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxc6iVcjLNrAN_siZy0wbcmDA8jWba07WFMyT3Hb8KCZEORKM29eOLHyxNn54LudvYrKw/exec'
+const form = document.forms['contact-forms']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(new FormData(form)); // Log the form data for debugging
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+        console.log('Success!', response);
+        alert("Thank you! your form is submitted successfully.");
+    })
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
+})
